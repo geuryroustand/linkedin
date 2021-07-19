17109442d7db27c84a6662c37595f4e767510e6f
Radensuaso <andrels9283@gmail.com>
2021-07-19 12:14:06 +0100

    Dashboard complete

--- src/Components/MePage/MePage.jsx
+++ src/Components/MePage/MePage.jsx
@@ -1,10 +1,13 @@
 import { Container, Row, Col } from "react-bootstrap"
+import YourDashboard from "./YourDashboard"
 
 const MePage = () => {
   return (
     <Container>
       <Row>
-        <Col xs={12} md={8}></Col>
+        <Col xs={12} md={8}>
+          <YourDashboard />
+        </Col>
         <Col xs={12} md={4}></Col>
       </Row>
     </Container>
