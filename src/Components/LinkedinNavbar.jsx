f1efd7f70fed48316e092b34fc252e090a7b9541
Radensuaso <andrels9283@gmail.com>
2021-07-19 09:58:33 +0100

    Further initialization done

---
+++ src/Components/LinkedinNavbar.jsx
@@ -0,0 +1,7 @@
+import { withRouter } from "react-router"
+
+const LinkedinNavbar = () => {
+  return <nav>navbar</nav>
+}
+
+export default withRouter(LinkedinNavbar)
---
+++ src/Components/MePage/MePage.jsx
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+const MePage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} sm={6} md={8}></Col>
+        <Col xs={12} sm={6} md={4}></Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default MePage
