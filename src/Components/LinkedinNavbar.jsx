fb6ec139fa580319064ef09dd8e62843a7f4381c
MagdalenaES83 <84572667+MagdalenaES83@users.noreply.github.com>
2021-07-20 15:43:28 +0200

    update
    updated Navbar, hover, Me-drop

--- src/Components/LinkedinNavbar.jsx
+++ src/Components/LinkedinNavbar.jsx
@@ -12,6 +12,8 @@ import {
   Form,
   NavDropdown,
   Image,
+  Card
+
 } from "react-bootstrap"
 import React from "react"
 import {
@@ -20,96 +22,101 @@ import {
   BsChatQuoteFill,
   BsBellFill,
   BsFillCalendarFill,
+  BsSearch
 } from "react-icons/bs"
 import { FaSuitcase } from "react-icons/fa"
 
 const LinkedinNavbar = () => {
   return (
-    <div id="linked-in-navbar" className="border-bottom">
+    <div id="linked-in-navbar" classNameName="border-bottom">
       <Container>
         <Row>
-          <Col xs={4}>
+          <Col className="ABC" xs={4}>
             {" "}
+            
             <Navbar>
-              <Navbar.Brand href="#home" id="NavBar">
-                <Form className="d-flex">
-                  <img
+              <Navbar.Brand href="#home" className="d-flex" >
+                <Image
+                    id="logoIN"
                     src="https://image.flaticon.com/icons/png/512/174/174857.png"
-                    width="38"
-                    height="38"
-                    className="d-inline-block align-top"
-                    alt="Linkedin logo"
-                    id="logo"
+                    width="36"
+                    height="36"
+                    className="mr-1"
                   />
+                  
+                  <Form >
+                   <Form.Group id="NavIco" className="d-flex" controlId="searchBAR">
+                  <Form.Label><BsSearch /></Form.Label>
                   <FormControl
                     type="search"
                     placeholder="Search"
-                    className="mr-2"
-                    aria-label="Search"
+                    classNameName="mr-2"
+                    aria-label=" Search "
+                     classNameName="SearchMe"
                   />
-                </Form>
+                </Form.Group></Form>
               </Navbar.Brand>
             </Navbar>
           </Col>
 
           <Col xs={1}>
-            <Row id="NavIco">
+            <Row className="NavIco">
               <Nav.Link href="#home">
                 <BsFillHouseDoorFill></BsFillHouseDoorFill>
               </Nav.Link>{" "}
             </Row>
-            <Row id="navn">
+            <Row className="navn">
               <Nav.Link href="#home"> Home</Nav.Link>
             </Row>
           </Col>
 
           <Col xs={1}>
-            <Row id="NavIco">
+            <Row className="NavIco">
               {" "}
-              <Nav.Link href="#network">
+              <Nav.Link className="NavIco" href="#network">
                 <BsPeopleFill></BsPeopleFill>{" "}
               </Nav.Link>
             </Row>
-            <Row id="navn">
+            <Row className="navn">
               <Nav.Link href="#network"> My Network</Nav.Link>
             </Row>
           </Col>
           <Col xs={1}>
-            <Row id="NavIco">
+            <Row className="NavIco">
               {" "}
               <Nav.Link href="#jobs">
                 <FaSuitcase></FaSuitcase>{" "}
               </Nav.Link>{" "}
             </Row>
-            <Row id="navn">
+            <Row className="navn">
               <Nav.Link href="#jobs"> Jobs</Nav.Link>
             </Row>
           </Col>
           <Col xs={1}>
-            <Row id="NavIco">
+            <Row className="NavIco">
               <Nav.Link href="#messaging">
                 {" "}
                 <BsChatQuoteFill></BsChatQuoteFill>
               </Nav.Link>
             </Row>
-            <Row id="navn">
+            <Row className="navn">
               <Nav.Link href="#messaging"> Messaging</Nav.Link>
             </Row>
           </Col>
           <Col xs={1}>
-            <Row id="NavIco">
+            <Row className="NavIco">
               <Nav.Link href="#notifications">
                 {" "}
                 <BsBellFill></BsBellFill>
               </Nav.Link>
             </Row>
-            <Row id="navn">
+            <Row className="navn">
               <Nav.Link href="#notifications"> Notification</Nav.Link>
             </Row>
           </Col>
 
           <Col xs={1}>
-            <Row id="NavIco">
+            <Row className="NavIco">
               <Nav.Link href="#Me">
                 <Col xs={6} md={4}>
                   <Image
@@ -123,8 +130,17 @@ const LinkedinNavbar = () => {
             <Row>
               {" "}
               <NavDropdown id="navnMe" title="Me">
-                <NavDropdown.Item id="dropMe" href="#action/3.2">
-                  <div>Profile information + picture </div>
+                <NavDropdown.Item id="meForm" href="#action/3.2">
+                  <Row> 
+                    <Col > <Image
+                    id="minion"
+                    src="https://1075koolfm.com/wp-content/uploads/2020/02/miniomn.jpg"
+                    roundedCircle
+                    className="d-flex"
+                  /></Col> 
+                    <Col id="" className="d-flex m-1" >NAME </Col>
+                    <Col className="d-flex m-1" >Strive School Student =><br></br> Full Stack Developer </Col>
+                  </Row>
                   <Button id="viewProf" variant="outline-primary">
                     View Profile
                   </Button>
@@ -161,7 +177,7 @@ const LinkedinNavbar = () => {
           </Col>
 
           <Col xs={1}>
-            <Row id="NavIco">
+            <Row className="NavIco">
               <Nav.Link href="#work">
                 <BsFillCalendarFill></BsFillCalendarFill>{" "}
               </Nav.Link>
@@ -170,16 +186,17 @@ const LinkedinNavbar = () => {
               {" "}
               <NavDropdown id="navnMe" title="Work">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+                <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
               </NavDropdown>
             </Row>
           </Col>
