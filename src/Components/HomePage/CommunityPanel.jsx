9b2f5387ce62ddffa5ad3c1ee7c9740f3ed08ffb
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-07-21 18:48:11 +0200

    little feature addition

--- src/Components/HomePage/CommunityPanel.jsx
+++ src/Components/HomePage/CommunityPanel.jsx
@@ -5,6 +5,8 @@ class CommunityPanel extends Component{
     render(){
         return(
             <div className='community-panel'>
+                <div className='p-3'>
+
                 <div className='mb-2'><a className='community-color ' href="/">Groups</a></div>
                 <div className='community-events '>
                     <header className='community-event-link'>
@@ -13,6 +15,7 @@ class CommunityPanel extends Component{
                     <div className='community-plus'><BiPlus/></div>
                 </div>
                 <div className='mb-2'><a className='community-color' href="">Followed Hashtags</a> </div>
+                </div>
                 <div className='community-discoverMore '>Discover more</div>
             </div>
         )
--- src/Components/HomePage/TopCourses.jsx
+++ src/Components/HomePage/TopCourses.jsx
@@ -8,21 +8,21 @@ class TopCourses extends Component{
         return(
             <div className='top-course '>
                 <div className='top-course-info mb-2'>
-                    <p className='top-course-title'>Today’s top courses</p>
+                    <p className='top-course-title pl-3'>Today’s top courses</p>
                     <span className='top-course-infoIcon'><HiInformationCircle/></span>
                 </div>
                 <div className='top-course-list'>
                       
                     <div className='top-course-author-link mb-2'>
-                        <p className='top-course-authorInfo'>
-                             <p className='top-course-author'>1. Digital Transformation</p>
-                             <p className='top-course-author-name'>Peter High</p>
+                        <p className='top-course-authorInfo pl-3'>
+                             <p className='top-course-author '>1. Digital Transformation</p>
+                             <p className='top-course-author-name '>Peter High</p>
                        </p>
                        
                         <p className='top-course-arrow'><BsBoxArrowUpRight/></p>
                     </div>
                     <div className='top-course-author-link mb-2'>
-                        <p className='top-course-authorInfo'>
+                        <p className='top-course-authorInfo pl-3'>
                              <p className='top-course-author'>2. Strive School</p>
                              <p className='top-course-author-name'>Tomas Berhane</p>
                        </p>
@@ -30,7 +30,7 @@ class TopCourses extends Component{
                         <p className='top-course-arrow'><BsBoxArrowUpRight/></p>
                     </div>
                     <div className='top-course-author-link mb-2'>
-                        <p className='top-course-authorInfo'>
+                        <p className='top-course-authorInfo pl-3'>
                              <p className='top-course-author'>2. Strive School</p>
                              <p className='top-course-author-name'>Stacey Gordon</p>
                        </p>
@@ -40,7 +40,7 @@ class TopCourses extends Component{
                    
                 </div>
                 <div>
-                    <p className='top-course-showMore'>Show more on Linkedin Learning <BiRightArrowAlt/> </p>
+                    <p className='top-course-showMore pl-3'>Show more on Linkedin Learning <BiRightArrowAlt/> </p>
                 </div>
             </div>
         )
